916a566602239ca8cd67fd7613376030a40fb894
Percell Gaskins <pmg8899@gmail.com>
2022-01-31 09:41:27 -0800

    commit

--- transformation.js
+++ transformation.js
@@ -1,2 +1,6 @@
 let form1 = document.getElementById("form1");
 let formDats = new FormData(form1);
+let submit = document.getElementsByClassName("submit1");
+function getData() {
+  submit.addEventListener("click", () => {});
+}
